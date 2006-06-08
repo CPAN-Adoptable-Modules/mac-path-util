2e08ed3cf9afc4e7eaecbcb9a91c6a8345406bbc
brian d foy <brian.d.foy@gmail.com>
2006-06-08 06:03:47 +0000

    Distro cleanup, added pod coverage
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1846 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Util.pm
+++ lib/Util.pm
@@ -357,7 +357,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright 2002-2004, brian d foy, All rights reserved
+Copyright 2002-2006, brian d foy, All rights reserved
 
 You may use this package under the same terms as Perl itself
 
